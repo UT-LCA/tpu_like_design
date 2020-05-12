f52c845986f01e3a83af3b4a81e0a0328f6611f3
Aman Arora <aarora1@mario.ece.utexas.edu>
2020-05-12 14:47:13 -0500

    Fixes to get ODIN to work

--- design/activation.v
+++ design/activation.v
@@ -59,18 +59,18 @@ end
 always @(address) begin
     for (i = 0; i < `MAT_MUL_SIZE; i=i+1) begin
     case (address[i])
-      4'b0000: data_slope[i] <= 8'd0;
-      4'b0001: data_slope[i] <= 8'd0;
-      4'b0010: data_slope[i] <= 8'd2;
-      4'b0011: data_slope[i] <= 8'd3;
-      4'b0100: data_slope[i] <= 8'd4;
-      4'b0101: data_slope[i] <= 8'd0;
-      4'b0110: data_slope[i] <= 8'd4;
-      4'b0111: data_slope[i] <= 8'd3;
-      4'b1000: data_slope[i] <= 8'd2;
-      4'b1001: data_slope[i] <= 8'd0;
-      4'b1010: data_slope[i] <= 8'd0;
-      default: data_slope[i] <= 8'd0;
+      4'b0000: data_slope[i][7:0] = 8'd0;
+      4'b0001: data_slope[i][7:0] = 8'd0;
+      4'b0010: data_slope[i][7:0] = 8'd2;
+      4'b0011: data_slope[i][7:0] = 8'd3;
+      4'b0100: data_slope[i][7:0] = 8'd4;
+      4'b0101: data_slope[i][7:0] = 8'd0;
+      4'b0110: data_slope[i][7:0] = 8'd4;
+      4'b0111: data_slope[i][7:0] = 8'd3;
+      4'b1000: data_slope[i][7:0] = 8'd2;
+      4'b1001: data_slope[i][7:0] = 8'd0;
+      4'b1010: data_slope[i][7:0] = 8'd0;
+      default: data_slope[i][7:0] = 8'd0;
     endcase  
     end
 end
@@ -79,18 +79,18 @@ end
 always @(address) begin
     for (i = 0; i < `MAT_MUL_SIZE; i=i+1) begin
     case (address[i])
-      4'b0000: data_intercept[i] <= 8'd127;
-      4'b0001: data_intercept[i] <= 8'd99;
-      4'b0010: data_intercept[i] <= 8'd46;
-      4'b0011: data_intercept[i] <= 8'd18;
-      4'b0100: data_intercept[i] <= 8'd0;
-      4'b0101: data_intercept[i] <= 8'd0;
-      4'b0110: data_intercept[i] <= 8'd0;
-      4'b0111: data_intercept[i] <= -8'd18;
-      4'b1000: data_intercept[i] <= -8'd46;
-      4'b1001: data_intercept[i] <= -8'd99;
-      4'b1010: data_intercept[i] <= -8'd127;
-      default: data_intercept[i] <= 8'd0;
+      4'b0000: data_intercept[i][7:0] = 8'd127;
+      4'b0001: data_intercept[i][7:0] = 8'd99;
+      4'b0010: data_intercept[i][7:0] = 8'd46;
+      4'b0011: data_intercept[i][7:0] = 8'd18;
+      4'b0100: data_intercept[i][7:0] = 8'd0;
+      4'b0101: data_intercept[i][7:0] = 8'd0;
+      4'b0110: data_intercept[i][7:0] = 8'd0;
+      4'b0111: data_intercept[i][7:0] = -8'd18;
+      4'b1000: data_intercept[i][7:0] = -8'd46;
+      4'b1001: data_intercept[i][7:0] = -8'd99;
+      4'b1010: data_intercept[i][7:0] = -8'd127;
+      default: data_intercept[i][7:0] = 8'd0;
     endcase  
     end
 end
@@ -99,40 +99,40 @@ end
 always @(inp_data) begin
     for (i = 0; i < `MAT_MUL_SIZE; i=i+1) begin
         if((inp_data[i*`DWIDTH +:`DWIDTH])>=90) begin
-           address[i] <= 4'b0000;
+           address[i][3:0] = 4'b0000;
         end
         else if ((inp_data[i*`DWIDTH +:`DWIDTH])>=39 && (inp_data[i*`DWIDTH +:`DWIDTH])<90) begin
-           address[i] <= 4'b0001;
+           address[i][3:0] = 4'b0001;
         end
         else if ((inp_data[i*`DWIDTH +:`DWIDTH])>=28 && (inp_data[i*`DWIDTH +:`DWIDTH])<39) begin
-           address[i] <= 4'b0010;
+           address[i][3:0] = 4'b0010;
         end
         else if ((inp_data[i*`DWIDTH +:`DWIDTH])>=16 && (inp_data[i*`DWIDTH +:`DWIDTH])<28) begin
-           address[i] <= 4'b0011;
+           address[i][3:0] = 4'b0011;
         end
         else if ((inp_data[i*`DWIDTH +:`DWIDTH])>=1 && (inp_data[i*`DWIDTH +:`DWIDTH])<16) begin
-           address[i] <= 4'b0100;
+           address[i][3:0] = 4'b0100;
         end
         else if ((inp_data[i*`DWIDTH +:`DWIDTH])==0) begin
-           address[i] <= 4'b0101;
+           address[i][3:0] = 4'b0101;
         end
         else if ((inp_data[i*`DWIDTH +:`DWIDTH])>-16 && (inp_data[i*`DWIDTH +:`DWIDTH])<=-1) begin
-           address[i] <= 4'b0110;
+           address[i][3:0] = 4'b0110;
         end
         else if ((inp_data[i*`DWIDTH +:`DWIDTH])>-28 && (inp_data[i*`DWIDTH +:`DWIDTH])<=-16) begin
-           address[i] <= 4'b0111;
+           address[i][3:0] = 4'b0111;
         end
         else if ((inp_data[i*`DWIDTH +:`DWIDTH])>-39 && (inp_data[i*`DWIDTH +:`DWIDTH])<=-28) begin
-           address[i] <= 4'b1000;
+           address[i][3:0] = 4'b1000;
         end
         else if ((inp_data[i*`DWIDTH +:`DWIDTH])>-90 && (inp_data[i*`DWIDTH +:`DWIDTH])<=-39) begin
-           address[i] <= 4'b1001;
+           address[i][3:0] = 4'b1001;
         end
         else if ((inp_data[i*`DWIDTH +:`DWIDTH])<=-90) begin
-           address[i] <= 4'b1010;
+           address[i][3:0] = 4'b1010;
         end
         else begin
-            address[i] <= 4'b0101;
+           address[i][3:0] = 4'b0101;
         end
     end
 end
