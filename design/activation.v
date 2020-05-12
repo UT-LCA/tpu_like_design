d84dfdaec2bd89bd99a105d853ac722acdf4dfc8
Aman Arora <aarora1@mario.ece.utexas.edu>
2020-05-12 14:59:17 -0500

    This time hoping ODIN will pass

--- design/activation.v
+++ design/activation.v
@@ -16,9 +16,9 @@ reg  out_data_valid;
 reg  [`MAT_MUL_SIZE*`DWIDTH-1:0] out_activation;
 integer i;
 
-reg [3:0] address[`MAT_MUL_SIZE-1:0];
-reg [7:0] data_slope[`MAT_MUL_SIZE-1:0];
-reg [7:0] data_intercept[`MAT_MUL_SIZE-1:0];
+reg [(`MAT_MUL_SIZE*4)-1:0] address;
+reg [(`MAT_MUL_SIZE*8)-1:0] data_slope;
+reg [(`MAT_MUL_SIZE*8)-1:0] data_intercept;
 
 // If the activation block is not enabled, just forward the input data
 assign out_data             = enable_activation ? out_activation    : inp_data;
@@ -35,7 +35,7 @@ always @(posedge clk) begin
        if(in_data_available) begin
            for (i = 0; i < `MAT_MUL_SIZE; i=i+1) begin
                if(activation_type==1'b1) begin // tanH
-                    out_activation[i*`DWIDTH +:`DWIDTH] <= data_slope[i] * inp_data[i*`DWIDTH +:`DWIDTH] + data_intercept[i];
+                    out_activation[i*`DWIDTH +:`DWIDTH] <= data_slope[i*8 +: 8] * inp_data[i*`DWIDTH +:`DWIDTH] + data_intercept[i*8 +: 8];
                end
                else begin // ReLU
                     out_activation[i*`DWIDTH +:`DWIDTH] <= inp_data[i*`DWIDTH-1] ? {`DWIDTH{1'b0}} : inp_data[i*`DWIDTH +:`DWIDTH];
@@ -58,19 +58,19 @@ end
 //LUT for the slope
 always @(address) begin
     for (i = 0; i < `MAT_MUL_SIZE; i=i+1) begin
-    case (address[i])
-      4'b0000: data_slope[i][7:0] = 8'd0;
-      4'b0001: data_slope[i][7:0] = 8'd0;
-      4'b0010: data_slope[i][7:0] = 8'd2;
-      4'b0011: data_slope[i][7:0] = 8'd3;
-      4'b0100: data_slope[i][7:0] = 8'd4;
-      4'b0101: data_slope[i][7:0] = 8'd0;
-      4'b0110: data_slope[i][7:0] = 8'd4;
-      4'b0111: data_slope[i][7:0] = 8'd3;
-      4'b1000: data_slope[i][7:0] = 8'd2;
-      4'b1001: data_slope[i][7:0] = 8'd0;
-      4'b1010: data_slope[i][7:0] = 8'd0;
-      default: data_slope[i][7:0] = 8'd0;
+    case (address[i*4+:4])
+      4'b0000: data_slope[i*8+:8] = 8'd0;
+      4'b0001: data_slope[i*8+:8] = 8'd0;
+      4'b0010: data_slope[i*8+:8] = 8'd2;
+      4'b0011: data_slope[i*8+:8] = 8'd3;
+      4'b0100: data_slope[i*8+:8] = 8'd4;
+      4'b0101: data_slope[i*8+:8] = 8'd0;
+      4'b0110: data_slope[i*8+:8] = 8'd4;
+      4'b0111: data_slope[i*8+:8] = 8'd3;
+      4'b1000: data_slope[i*8+:8] = 8'd2;
+      4'b1001: data_slope[i*8+:8] = 8'd0;
+      4'b1010: data_slope[i*8+:8] = 8'd0;
+      default: data_slope[i*8+:8] = 8'd0;
     endcase  
     end
 end
@@ -78,19 +78,19 @@ end
 //LUT for the intercept
 always @(address) begin
     for (i = 0; i < `MAT_MUL_SIZE; i=i+1) begin
-    case (address[i])
-      4'b0000: data_intercept[i][7:0] = 8'd127;
-      4'b0001: data_intercept[i][7:0] = 8'd99;
-      4'b0010: data_intercept[i][7:0] = 8'd46;
-      4'b0011: data_intercept[i][7:0] = 8'd18;
-      4'b0100: data_intercept[i][7:0] = 8'd0;
-      4'b0101: data_intercept[i][7:0] = 8'd0;
-      4'b0110: data_intercept[i][7:0] = 8'd0;
-      4'b0111: data_intercept[i][7:0] = -8'd18;
-      4'b1000: data_intercept[i][7:0] = -8'd46;
-      4'b1001: data_intercept[i][7:0] = -8'd99;
-      4'b1010: data_intercept[i][7:0] = -8'd127;
-      default: data_intercept[i][7:0] = 8'd0;
+    case (address[i*4+:4])
+      4'b0000: data_intercept[i*8+:8] = 8'd127;
+      4'b0001: data_intercept[i*8+:8] = 8'd99;
+      4'b0010: data_intercept[i*8+:8] = 8'd46;
+      4'b0011: data_intercept[i*8+:8] = 8'd18;
+      4'b0100: data_intercept[i*8+:8] = 8'd0;
+      4'b0101: data_intercept[i*8+:8] = 8'd0;
+      4'b0110: data_intercept[i*8+:8] = 8'd0;
+      4'b0111: data_intercept[i*8+:8] = -8'd18;
+      4'b1000: data_intercept[i*8+:8] = -8'd46;
+      4'b1001: data_intercept[i*8+:8] = -8'd99;
+      4'b1010: data_intercept[i*8+:8] = -8'd127;
+      default: data_intercept[i*8+:8] = 8'd0;
     endcase  
     end
 end
@@ -99,40 +99,40 @@ end
 always @(inp_data) begin
     for (i = 0; i < `MAT_MUL_SIZE; i=i+1) begin
         if((inp_data[i*`DWIDTH +:`DWIDTH])>=90) begin
-           address[i][3:0] = 4'b0000;
+           address[i*4+:4] = 4'b0000;
         end
         else if ((inp_data[i*`DWIDTH +:`DWIDTH])>=39 && (inp_data[i*`DWIDTH +:`DWIDTH])<90) begin
-           address[i][3:0] = 4'b0001;
+           address[i*4+:4] = 4'b0001;
         end
         else if ((inp_data[i*`DWIDTH +:`DWIDTH])>=28 && (inp_data[i*`DWIDTH +:`DWIDTH])<39) begin
-           address[i][3:0] = 4'b0010;
+           address[i*4+:4] = 4'b0010;
         end
         else if ((inp_data[i*`DWIDTH +:`DWIDTH])>=16 && (inp_data[i*`DWIDTH +:`DWIDTH])<28) begin
-           address[i][3:0] = 4'b0011;
+           address[i*4+:4] = 4'b0011;
         end
         else if ((inp_data[i*`DWIDTH +:`DWIDTH])>=1 && (inp_data[i*`DWIDTH +:`DWIDTH])<16) begin
-           address[i][3:0] = 4'b0100;
+           address[i*4+:4] = 4'b0100;
         end
         else if ((inp_data[i*`DWIDTH +:`DWIDTH])==0) begin
-           address[i][3:0] = 4'b0101;
+           address[i*4+:4] = 4'b0101;
         end
         else if ((inp_data[i*`DWIDTH +:`DWIDTH])>-16 && (inp_data[i*`DWIDTH +:`DWIDTH])<=-1) begin
-           address[i][3:0] = 4'b0110;
+           address[i*4+:4] = 4'b0110;
         end
         else if ((inp_data[i*`DWIDTH +:`DWIDTH])>-28 && (inp_data[i*`DWIDTH +:`DWIDTH])<=-16) begin
-           address[i][3:0] = 4'b0111;
+           address[i*4+:4] = 4'b0111;
         end
         else if ((inp_data[i*`DWIDTH +:`DWIDTH])>-39 && (inp_data[i*`DWIDTH +:`DWIDTH])<=-28) begin
-           address[i][3:0] = 4'b1000;
+           address[i*4+:4] = 4'b1000;
         end
         else if ((inp_data[i*`DWIDTH +:`DWIDTH])>-90 && (inp_data[i*`DWIDTH +:`DWIDTH])<=-39) begin
-           address[i][3:0] = 4'b1001;
+           address[i*4+:4] = 4'b1001;
         end
         else if ((inp_data[i*`DWIDTH +:`DWIDTH])<=-90) begin
-           address[i][3:0] = 4'b1010;
+           address[i*4+:4] = 4'b1010;
         end
         else begin
-           address[i][3:0] = 4'b0101;
+           address[i*4+:4] = 4'b0101;
         end
     end
 end
